9d64b2b3cef6285e195217a1297d86cd2c0f74c6
Daro Omwanor <osirisdaro@gmail.com>
2022-05-26 12:36:45 -0700

    server.js

--- frontend/server.js
+++ frontend/server.js
@@ -9,9 +9,10 @@ var db = new sqlite3.Database('./db/vos.db');
 app.get('/', function(req,res){
 	sql = "SELECT * FROM citiesTable";
 	data = []
-	db.get(sql, function(err, row) {
-		console.log({'city':row.cityName, 'country':row.country});
-		data.push({'city':row.cityName, 'country':row.country});
+	db.get(sql, function(err, rows) {
+		rows.forEach(row){
+			console.log(row);
+		}
 	});
 	res.render('index', {'data': data});
 });
