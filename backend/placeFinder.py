9cda0f79124c1628956c521da3cb11b67ed73768
Daro Omwanor <osirisdaro@gmail.com>
2022-05-26 08:58:52 -0700

    Update

--- backend/placeFinder.py
+++ backend/placeFinder.py
@@ -10,6 +10,7 @@ import logging
 import websockets
 from websockets import WebSocketServerProtocol
 import math
+from pyvirtualdisplay import Display
 import pymysql.cursors
 from random import randint
 from selenium import webdriver
@@ -25,36 +26,47 @@ logging.basicConfig(level=logging.INFO)
 class placeFinder(object):
 
 	def googler(self, query):
+		display = Display(visible=0, size=(1200, 1200))
+		display.start()
 		try:
 			chrome_options = webdriver.ChromeOptions()
 			chrome_options.add_argument('--no-sandbox')
-			browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
+			browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 			q = urllib.parse.quote_plus(query)
 			search = 'https://www.google.com/search?tbs=lf:1,lf_ui:9&tbm=lcl&q='+q
 			browser.get(search)
 			scrapedValues = htmlObject(browser)
-			pdata = []
-			for x in scrapedValues.div_tag:
-				pdata.append(x.text)
-			pdF = pd.DataFrame(pdata, columns=['div_data'])
-			no_dup = pdF.drop_duplicates(subset=['div_data'])
-			scrapedData = []
-			for x in no_dup['div_data'].values:
-				splitLine = x.split('\n')
-				if len(splitLine) > 4 and len(splitLine) < 9:
-					scrapedData.append(self.dictListData(x))
-			browser.quit()
-			return scrapedData
+			ele = browser.find_elements_by_class_name("rllt__details")
+			places = []
+			for k in ele:
+				txt = k.text
+				places.append(self.dictListData(txt))
+			print(places)
+			return places
 		finally:
+			display.stop()
+			os.popen("pkill Chrome")
 			logging.info('Scraped For '+f'{query}')
 
 	def dictListData(self, str_input):
-		split_input = str_input.split('\n')
-		return {'Name': split_input[0], 
-				'Ratings': split_input[1],
-				'Tag': split_input[2],
-				'Address':split_input[3],
-				'Description': split_input[4],}
+		res = {}
+		try:
+			split_input = str_input.split('\n')
+			if len(split_input) > 4:
+				res = {'Name': split_input[0], 
+						'Ratings': split_input[1],
+						'Tag': split_input[2],
+						'Address':split_input[3],
+						'Description': split_input[4],}
+			else:
+				res = {'Name': split_input[0], 
+						'Ratings': split_input[1],
+						'Tag': split_input[2],
+						'Address':split_input[3],
+						'Description': "None",}
+		finally:
+			return res
+
 
 class htmlObject(PageObject):
 	# :Page Object
@@ -65,6 +77,7 @@ class htmlObject(PageObject):
     #span_tag = MultiPageElement(tag_name='span')
 
 if __name__ == '__main__':
-	for x in ["nightclubs","gentlemen clubs"]:
-		res = placeFinder("Las Vegas " + x)
+	pf = placeFinder()
+	for x in ["nightclubs","Bars"]:
+		res = pf.googler("Las Vegas " + x)
 		print(res)
