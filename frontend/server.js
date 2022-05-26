4c99e61d95fec06194a53c46a780bf7f8eeda77e
Daro Omwanor <osirisdaro@gmail.com>
2022-05-26 12:10:41 -0700

    server.js

--- frontend/server.js
+++ frontend/server.js
@@ -4,11 +4,15 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('/home/ubuntu/dgnx.io/frontend/db/vos.db');
-console.log(db)
+var db = new sqlite3.Database('./db/vos.db');
+
 app.get('/', function(req,res){
 	sql = `SELECT * FROM citiesTable`;
-	var data = db.all(sql);
+	db.run(sql, function(err, rows){
+		rows.forEach(function(row){
+			console.log(row);
+		})
+	});
 	console.log(data)
 	res.render('index', {'data': data});
 });
