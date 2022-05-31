6925d68848ca222a37b430a75e199eaaa64c52d8
Daro Omwanor <osirisdaro@gmail.com>
2022-05-31 12:27:34 -0700

    Added Crawler event

---
+++ backend/crawler.py
@@ -0,0 +1,78 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+import pandas as pd
+import urllib.parse
+import asyncio
+import uuid
+import json
+import logging
+import websockets
+from websockets import WebSocketServerProtocol
+import math
+from pyvirtualdisplay import Display
+import pymysql.cursors
+from random import randint
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
+from page_objects import PageObject, MultiPageElement, PageElement
+import pysqlite3
+
+
+conn = sqlite3.connect("/home/ubuntu/dgnx.io/frontend/db/vos.db")
+
+logging.basicConfig(level=logging.INFO)
+
+class placeFinder(object):
+
+	def googler(self, query, city, placeType):
+		display = Display(visible=0, size=(1200, 1200))
+		display.start()
+		try:
+			chrome_options = webdriver.ChromeOptions()
+			chrome_options.add_argument('--no-sandbox')
+			browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
+			q = urllib.parse.quote_plus(query)
+			search = 'https://www.google.com/search?tbs=lf:1,lf_ui:9&tbm=lcl&q='+q
+			browser.get(search)
+			ele = browser.find_elements_by_class_name("rllt__details")
+			places = []
+			for k in ele:
+				txt = k.text
+				self.uploadToDB(city, placeType, self.dictListData(txt))
+				places.append(self.dictListData(txt))
+			print(places)
+			return places
+		finally:
+			display.stop()
+			os.popen("pkill Chrome")
+			logging.info('Scraped For '+f'{query}')
+	def uploadToDB(self,place,city, placeType):
+		guid = str(uuid.uuid4())
+		query = "INSERT INTO placesTable(guid, city, placeType, placeName, ratings, reviews) VALUES({0},{1},{2},{3},{4})".format(guid, city, placeType, place['Name'], place['Ratings'], place['Tag'])
+		cur = conn.cursor()
+		cur.execute()
+
+	def dictListData(self, str_input):
+		res = {}
+		try:
+			split_input = str_input.split('\n')
+			if len(split_input) > 4:
+				res = {'Name': split_input[0], 
+						'Ratings': split_input[1],
+						'Tag': split_input[2],
+						'Address':split_input[3],
+						'Description': split_input[4],}
+			else:
+				res = {'Name': split_input[0], 
+						'Ratings': split_input[1],
+						'Tag': split_input[2],
+						'Address':split_input[3],
+						'Description': "None",}
+		finally:
+			return res
