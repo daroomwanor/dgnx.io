c01de45bdd8f6d61a13f5baf4f5d9d835c486c0f
Daro Omwanor <osirisdaro@gmail.com>
2022-05-31 12:59:34 -0700

    Update Crawler

--- backend/crawler.py
+++ backend/crawler.py
@@ -57,7 +57,7 @@ class placeFinder(object):
 			query = "INSERT INTO placesTable(guid, city, placeType, placeName, ratings, reviews) VALUES(?,?,?,?,?,?)"
 			cur = conn.cursor()
 			print(query)
-			cur.execute(query, (guid, city, placeType, place['Name'], place['Ratings'], place['Reviews']))
+			cur.execute(query, (guid, city, placeType, place['Name'], place['Ratings'], place['Tag']))
 		except (RuntimeError, TypeError, NameError, pysqlite3.OperationalError) as e:
 			print(e)
 
