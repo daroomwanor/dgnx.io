108e272a8ab5c672c46bcf33b9553391cbd3b671
Daro Omwanor <osirisdaro@gmail.com>
2022-05-31 21:16:52 -0700

    Update crawler

--- backend/crawler.py
+++ backend/crawler.py
@@ -31,9 +31,9 @@ logging.basicConfig(level=logging.INFO)
 class placeFinder(object):
 
 	def googler(self,city, placeType):
-		display = Display(visible=0, size=(1200, 1200))
-		display.start()
 		try:
+			display = Display(visible=0, size=(1200, 1200))
+			display.start()
 			chrome_options = webdriver.ChromeOptions()
 			chrome_options.add_argument('--no-sandbox')
 			browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
@@ -47,11 +47,10 @@ class placeFinder(object):
 				thumbnails = imgs[k].get_attribute('src')
 				place = self.dictListData(ele[k].text)
 				self.uploadToDB(placeType,city,place,thumbnails)
-				places.append(place)
-			return places
-		finally:
 			display.stop()
 			os.popen("pkill Chrome")
+		finally:
+			pass
 
 	def isPlaceFound(self, placeName, city):
 		try:
