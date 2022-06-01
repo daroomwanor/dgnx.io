df4a6e157fe196b91f11910ee141c0fdc0cc6e2d
Daro Omwanor <osirisdaro@gmail.com>
2022-05-31 17:58:51 -0700

    Update crawler

--- backend/crawler.py
+++ backend/crawler.py
@@ -39,7 +39,7 @@ class placeFinder(object):
 			chrome_options = webdriver.ChromeOptions()
 			chrome_options.add_argument("start-maximized")
 			chrome_options.add_argument('--no-sandbox')
-			browser = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
+			browser = webdriver.Chrome(options=chrome_options, service=Service(path))
 			q = urllib.parse.quote_plus(placeType+" "+city)
 			search = 'https://www.google.com/search?tbs=lf:1,lf_ui:9&tbm=lcl&q='+q
 			browser.get(search)
