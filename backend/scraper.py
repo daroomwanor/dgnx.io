f950c33e95d72c820977ee6dfff8d3de6ee93815
Daro Omwanor <osirisdaro@gmail.com>
2022-06-01 19:11:52 -0700

    Added A Scraper

--- backend/scraper.py
+++ backend/scraper.py
@@ -66,9 +66,6 @@ class placeFinder(object):
 			cur = conn.cursor()
 			cur.execute(query, (address, phone, website, description, hours, guid))
 			conn.commit()
-			print(cur.lastrowid)
-			return cur.lastrowid
-			print("Logged")
 		except (RuntimeError, TypeError, NameError, pysqlite3.OperationalError,KeyError) as e:
 			print(e)
 		finally:
@@ -129,19 +126,23 @@ if __name__ == '__main__':
 		for place in places:
 			try:
 				data = pf.googler(place[1],place[2])
+				print(palce[2])
 				print(data)
-				if "Phone" not in data.keys():
-					data['Phone'] = None
-				if "Address" not in data.keys():
-					data['Address'] = None
-				if "Website" not in data.keys():
-					data['Website'] = None
-				if "Info" not in data.keys():
-					data['Info'] = None
-				if "Hours" not in data.keys():
-					data['Hours'] = None
-				
-				pf.uploadToDB(data['Address'], data['Phone'], data['Website'], data['Info'], data['Hours'], place[0])
-				time.sleep(2.0)
+				if data is not None:
+					if "Phone" not in data.keys():
+						data['Phone'] = None
+					if "Address" not in data.keys():
+						data['Address'] = None
+					if "Website" not in data.keys():
+						data['Website'] = None
+					if "Info" not in data.keys():
+						data['Info'] = None
+					if "Hours" not in data.keys():
+						data['Hours'] = None
+					
+					pf.uploadToDB(data['Address'], data['Phone'], data['Website'], data['Info'], data['Hours'], place[0])
+					time.sleep(2.0)
+			except (RuntimeError, TypeError, NameError, pysqlite3.OperationalError,KeyError) as e:
+				pass
 			finally:
 				pass
