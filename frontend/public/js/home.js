9e77749d318ac74c49b53624eaedf3b8fa89cf80
Daro Omwanor <osirisdaro@gmail.com>
2022-10-16 06:39:27 -0700

    Update

--- frontend/public/js/home.js
+++ frontend/public/js/home.js
@@ -1,11 +1,325 @@
+
 function getPlaces(){
 	var city = document.getElementById('cities').value;
 	var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("trip").value = "Select Trip";
        		document.getElementById("main").innerHTML = xhttp.responseText;
     	}
 	};
 	xhttp.open("GET", "http://127.0.0.1:8080/findPlaces?city="+city, true);
 	xhttp.send();
+	clearTab()
 }
+
+function clearTab() {
+	tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-gray", "");
+    }
+    document.getElementById("foodTab").className += " w3-gray";
+}
+
+
+function addPlace(e) {
+	var placeId = e.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+       		document.getElementById("main_modal").innerHTML = xhttp.responseText;
+       		document.getElementById("main_modal").style.display = "Block";
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/getPlaceInfo?placeId="+placeId+"&userId="+userId, true);
+	xhttp.send();
+}
+
+
+function displayTrip(){
+
+	var tripId = document.getElementById("selectTrip").value;
+	if (tripId == "New"){
+		document.getElementById("displayAction").style.display = "grid";
+		document.getElementById("addTrip").style.display = "None";
+	}else{
+		if (tripId != ""){
+			document.getElementById("displayAction").style.display = "None";
+			document.getElementById("addTrip").style.display = "grid";
+		}else{
+			document.getElementById("displayAction").style.display = "None";
+			document.getElementById("addTrip").style.display = "None";
+		}
+	}; 
+}
+
+
+function createItinerary(evt) {
+	var placeId = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var tripName = document.getElementById("tripName").value;
+	tripName = escape(tripName);
+	console.log(tripName);
+	var reserveStartTime = document.getElementById("reserveStartTime").value;
+	var reserveEndTime = document.getElementById("reserveEndTime").value;
+	var start_am_pm = document.getElementById("start_am_pm").value;
+	var end_am_pm = document.getElementById("end_am_pm").value;
+	var reserveDay = document.getElementById("reserveDay").value;
+	var reserveMonth = document.getElementById("reserveMonth").value;
+	var reserveYear = document.getElementById("reserveYear").value;
+	var startDay = document.getElementById("startDay").value;
+	var startMonth = document.getElementById("startMonth").value;
+	var startYear = document.getElementById("startYear").value;
+	var endDay = document.getElementById("endDay").value;
+	var endMonth = document.getElementById("endMonth").value;
+	var endYear = document.getElementById("endYear").value;
+	var comment = escape(document.getElementById("comment").value);
+	var data = {
+		tripName:tripName,
+		reserveStartTime:reserveStartTime,
+		reserveEndTime:reserveEndTime,
+		start_am_pm:start_am_pm,
+		end_am_pm:end_am_pm, 
+		reserveDay:reserveDay, 
+		reserveMonth:reserveMonth, 
+		reserveYear:reserveYear, 
+		startDay:startDay, 
+		startMonth:startMonth, 
+		startYear:startYear, 
+		endDay:endDay, 
+		endMonth:endMonth, 
+		endYear:endYear, 
+		comment:comment
+	}
+	var isTrip = document.getElementById("selectTrip").value;
+	if (isTrip == "New"){
+		var xhttp = new XMLHttpRequest();
+	    xhttp.onreadystatechange = function() {
+	    	if (this.readyState == 4 && this.status == 200) {
+	    		var resp = JSON.parse(xhttp.responseText);
+	    		console.log(resp);
+	    		window.location.href = "/?userId="+resp['userId'];
+	    	}
+		};
+		xhttp.open("GET", "http://127.0.0.1:8080/createItineraryAction?placeId="+placeId+"&userId="+userId+"&data="+JSON.stringify(data), true);
+		xhttp.send();
+	}
+}
+
+function displayItinerary() {
+	var tripId = document.getElementById("trip").value;
+	console.log(tripId);
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+
+    		document.getElementById("cities").value = "Choose a city";
+    		document.getElementById("tabBar").style.display = "None";
+       		document.getElementById("main").innerHTML = xhttp.responseText;
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/displayTripItinerary?tripId="+tripId, true);
+	xhttp.send();
+	clearTab()
+}
+
+function addTripToItinerary(evt){
+
+	var placeId = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var reserveStartTime = document.getElementById("reserveStartTime").value;
+	var reserveEndTime = document.getElementById("reserveEndTime").value;
+	var start_am_pm = document.getElementById("start_am_pm").value;
+	var end_am_pm = document.getElementById("end_am_pm").value;
+	var reserveDay = document.getElementById("reserveDay").value;
+	var reserveMonth = document.getElementById("reserveMonth").value;
+	var reserveYear = document.getElementById("reserveYear").value;
+	var comment = escape(document.getElementById("comment").value);
+	var tripId= document.getElementById("selectTrip").value;
+	var data = {
+		placeId:placeId,
+		reserveStartTime:reserveStartTime,
+		reserveEndTime:reserveEndTime, 
+		comment:comment,
+		reserveDay: reserveDay,
+		reserveMonth:reserveMonth,
+		reserveYear:reserveYear,
+		start_am_pm:start_am_pm,
+		end_am_pm:end_am_pm
+	}
+	console.log(data);
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("trip").value = tripId;
+    		displayItinerary()
+       		document.getElementById("main_modal").style.display = "None";
+       		document.getElementById("trip").value = "Select Trip";
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/addPlaceToItinerary?userId="+userId+"&data="+JSON.stringify(data)+"&tripId="+tripId, true);
+	xhttp.send();
+}
+
+function editTripItinerary(evt){
+
+	var itemGuid = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var placeId = document.getElementById("placeId").value;
+	var reserveStartTime = document.getElementById("reserveStartTime").value;
+	var start_am_pm = document.getElementById("start_am_pm").value;
+	var end_am_pm = document.getElementById("end_am_pm").value;
+	var reserveEndTime = document.getElementById("reserveEndTime").value;
+	var reserveDay = document.getElementById("reserveDay").value;
+	var reserveMonth = document.getElementById("reserveMonth").value;
+	var reserveYear = document.getElementById("reserveYear").value;
+	var comment = escape(document.getElementById("comment").value);
+	var tripGuid= document.getElementById("tripGuid").value;
+	var data = {
+		tripGuid:tripGuid,
+		placeId:placeId,
+		reserveStartTime:reserveStartTime,
+		reserveEndTime:reserveEndTime, 
+		comment:comment,
+		reserveDay: reserveDay,
+		reserveMonth:reserveMonth,
+		reserveYear:reserveYear,
+		start_am_pm:start_am_pm,
+		end_am_pm:end_am_pm
+	}
+	console.log(data);
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("trip").value = tripGuid;
+    		displayItinerary()
+       		document.getElementById("main_modal").style.display = "None";
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/editTripItinerary?userId="+userId+"&data="+JSON.stringify(data)+"&itemGuid="+itemGuid, true);
+	xhttp.send();
+}
+
+function editTripAction(){
+	document.getElementById("tripTableDisplay").style.display = "none";
+	document.getElementById("tripTableEdit").style.display = "block";
+	document.getElementById("editTripButton").style.display = "none";
+	document.getElementById("deleteTripButton").style.display = "none";
+}
+
+function closeTripEdit(){
+	document.getElementById("tripTableDisplay").style.display = "block";
+	document.getElementById("tripTableEdit").style.display = "none";
+	document.getElementById("editTripButton").style.display = "block";
+	document.getElementById("deleteTripButton").style.display = "block";
+}
+
+function updateTripAction(evt){
+	var userId = document.getElementById("userId").value;
+	var guid = evt.getAttribute("data-record");
+	var tripName = document.getElementById("tripName").value;
+	var startDay = document.getElementById("startDay").value;
+	var startMonth = document.getElementById("startMonth").value;
+	var startYear = document.getElementById("startYear").value;
+	var endDay = document.getElementById("endDay").value;
+	var endMonth = document.getElementById("endMonth").value;
+	var endYear = document.getElementById("endYear").value;
+	var data = {
+		tripName: tripName,
+		startDay:startDay, 
+		startMonth:startMonth, 
+		startYear:startYear, 
+		endDay:endDay, 
+		endMonth:endMonth, 
+		endYear:endYear
+	};
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("trip").value = guid;
+			displayItinerary();
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/updateTripsTable?userId="+userId+"&data="+JSON.stringify(data)+"&guid="+guid, true);
+	xhttp.send();
+}
+
+function displayTripItemAction(evt) {
+	var userId = document.getElementById("userId").value;
+	var id = evt.getAttribute('data-record');
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("main_modal").innerHTML = xhttp.responseText;
+       		document.getElementById("main_modal").style.display = "Block";
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/displayTripItem?userId="+userId+"&id="+id, true);
+	xhttp.send();
+
+}
+
+
+function deleteTripAction(evt){
+	var tripGuid = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		document.getElementById("main_modal").innerHTML = xhttp.responseText;
+       		document.getElementById("main_modal").style.display = "Block";
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/selectTripView?userId="+userId+"&tripGuid="+tripGuid, true);
+	xhttp.send();
+
+}
+
+function deleteTrip(evt){
+	var tripGuid = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		var response = JSON.parse(xhttp.responseText);
+    		console.log(response);
+    		if (response['status'] == "success"){
+    			document.getElementById("main_modal").style.display = "None";
+       			window.location.href = "/?userId="+response["userId"];
+    		}
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/deleteTripAction?userId="+userId+"&tripGuid="+tripGuid, true);
+	xhttp.send();
+}
+
+
+function deleteItineraryItem(evt){
+	var itemGuid = evt.getAttribute("data-record");
+	var userId = document.getElementById("userId").value;
+	var tripGuid= document.getElementById("tripGuid").value;
+	var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+    		var response = JSON.parse(xhttp.responseText);
+    		console.log(response);
+    		if (response['status'] == "success"){
+    			document.getElementById("trip").value = tripGuid;
+    			document.getElementById("main_modal").style.display = "None";
+       			displayItinerary();
+    		}
+    	}
+	};
+	xhttp.open("GET", "http://127.0.0.1:8080/deleteItineraryItem?userId="+userId+"&itemGuid="+itemGuid, true);
+	xhttp.send();
+}
+
+
+
+
+
+
+
+
+
--- frontend/server.js
+++ frontend/server.js
@@ -1,28 +1,171 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const router  = express.Router();
+const app = express();
 
-const http = require('http');
-const url = require('url');
-
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./db/vos.db');
 
-app.get('/', function(req,res){
+var uuid = require('uuid');
+const http = require('http');
+const url = require('url');
+var sess;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret:"guid", saveUninitialized:true, resave:true}));
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+router.get('/', function(request,response){
+	var req = url.parse(request.url, true).query;
+	sess = request.session;
+	trips = [];
+	if (req.userId != undefined){
+		sql = "SELECT * FROM tripsTable WHERE userGuid = ?";
+		db.all(sql, req.userId, function(err, rows) {
+			if (rows.length > 0){
+				rows.forEach(function(row){
+					trips.push(row);
+				});
+			}
+			console.log(err);
+		});
+	}
 	sql = "select distinct(city) from placesTable order by city ASC";
 	cities = [];
 	db.all(sql, function(err, rows) {
 		rows.forEach(function(row){
 			cities.push(row);
 		});
-		console.log(cities)
-		res.render('home', {'cities': cities});
+		response.render('home', {'cities': cities, 'trips':trips});
 	});
 
 });
 
-app.get('/findPlaces', function(request,response){
+
+router.get('/displayTripItinerary', function(request, response){
+	sess = request.session;
+	var req = url.parse(request.url, true).query;
+	var sql = 'SELECT * FROM itineraryTable as itin LEFT JOIN placesTable as place ON place.Id = itin.placeId LEFT JOIN tripsTable as trip ON trip.tripGuid = itin.tripGuid  WHERE trip.tripGuid = ?';
+	var places = [];
+	var city = "";
+	db.all(sql, req.tripId, function(err, rows) {
+		if(rows.length > 0){
+			city = rows[0]["city"]; 
+			rows.forEach(function(row){
+				places.push(row);
+			});
+		}
+		db.all('SELECT * FROM placesTable WHERE city = ?', city, function(err,rows){
+			console.log(rows);
+			response.render('itinerary', {'data': places, 'new_data':rows});
+		});
+	});
+
+});
+
+router.get('/addPlaceToItinerary', function(request, response){
+	sess = request.session;
+	const queryObject = url.parse(request.url, true).query;
+	var jsonData = JSON.parse(queryObject.data);
+	var tripGuid = queryObject.tripId;
+	var itemGuid = uuid.v4();
+	var placeId = jsonData['placeId'];
+	var reserveStartTime = jsonData['reserveStartTime'];
+	var reserveEndTime = jsonData['reserveEndTime'];
+	var start_am_pm = jsonData['start_am_pm'];
+	var end_am_pm = jsonData['end_am_pm'];
+	var start_am_pm = jsonData['start_am_pm'];
+	var end_am_pm = jsonData['end_am_pm'];
+	var reserveDay = jsonData['reserveDay'];
+	var reserveMonth = jsonData['reserveMonth'];
+	var reserveYear = jsonData['reserveYear'];
+	var comment = jsonData['comment'];
+	console.log(jsonData);
+	var insert_into_itinerary = "INSERT INTO itineraryTable (tripGuid, placeId, itemGuid, reserveStartTime, reserveEndTime,start_am_pm, end_am_pm, reserveDay, reserveMonth, reserveYear, placeInfo) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+	db.run(insert_into_itinerary, [tripGuid, placeId, itemGuid, reserveStartTime, reserveEndTime,start_am_pm, end_am_pm, reserveDay,reserveMonth,reserveYear,comment], function(err, rows) {
+		if(err){
+			console.log(err);
+			response.write("Error");
+			response.send();
+		}else{
+			response.write(JSON.stringify({"response":"success",}));
+			response.send();
+		}
+	});
+
+
+});
+
+router.get('/editTripItinerary', function(request, response){
+	sess = request.session;
+	const queryObject = url.parse(request.url, true).query;
+	var jsonData = JSON.parse(queryObject.data);
+	var tripGuid = jsonData['tripGuid'];
+	var itemGuid = queryObject.itemGuid;
+	var placeId = jsonData['placeId'];
+	var reserveStartTime = jsonData['reserveStartTime'];
+	var reserveEndTime = jsonData['reserveEndTime'];
+	var start_am_pm = jsonData['start_am_pm'];
+	var end_am_pm = jsonData['end_am_pm'];
+	var reserveDay = jsonData['reserveDay'];
+	var reserveMonth = jsonData['reserveMonth'];
+	var reserveYear = jsonData['reserveYear'];
+	var userId = queryObject.userId;
+	var comment = jsonData['comment'];
+	console.log(jsonData);
+	update_sql = "UPDATE itineraryTable SET tripGuid =?,placeId=?,reserveStartTime=?,reserveEndTime=?,start_am_pm=?, end_am_pm=?,reserveDay=?,reserveMonth=?,reserveYear=?,placeInfo=? WHERE itemGuid=?";
+	db.run(update_sql, [tripGuid, placeId, reserveStartTime, reserveEndTime,start_am_pm, end_am_pm,reserveDay,reserveMonth,reserveYear,comment, itemGuid], function(err, rows) {
+		if(err){
+			console.log(err);
+			response.write(JSON.stringify({"response":"failed", "userId":userId}));
+			response.send();
+		}else{
+			response.write(JSON.stringify({"response":"success", "userId":userId}))
+			response.send();
+		}
+	});
+
+
+});
+
+router.post('/login', function(request, response) {
+	var email = request.body.email;
+	var password = request.body.pass;
+	sess = request.session;
+	sess.email = email;
+	response.write("Found")
+});
+
+router.get('/getPlaceInfo', function (request, response) {
+	sess = request.session;
+	var req = url.parse(request.url, true).query;
+	var placeId = req.placeId;
+	var userId = req.userId;
+	sql = "SELECT * FROM tripsTable WHERE userGuid = ?";
+	trips = [];
+	db.all(sql, userId, function(err, rows) {
+		if (rows.length > 0){
+			rows.forEach(function(row){
+				trips.push(row);
+			});
+		}
+	});
+	sql = 'SELECT * FROM placesTable WHERE Id = ?';
+	place= [];
+	db.all(sql,placeId, function(error, rows){
+		rows.forEach(function(row){
+			place.push(row);
+		});
+		response.render('displayPlace', {'data':[place, trips]});
+	});
+	
+});
+
+router.get('/findPlaces', function(request,response){
 	const queryObject = url.parse(request.url, true).query;
 	city = queryObject.city;
 	sql = 'SELECT * FROM placesTable WHERE city = ?';
@@ -35,16 +178,170 @@ app.get('/findPlaces', function(request,response){
 	});
 });
 
-app.get('/backpage', function(request, response){
-	sql = 'select distinct(city) from placesTable order by city ASC';
-	cities= []
-	db.all(sql, function(error, rows){
-		rows.forEach(function(row){
-			cities.push(row);
+router.get("/createItineraryAction", function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var jsonData = JSON.parse(queryObject.data);
+	var placeId = queryObject.placeId;
+	var userId = queryObject.userId;
+	var tripGuid = uuid.v4();
+	var startDay = jsonData['startDay'];
+	var startMonth = jsonData['startMonth'];
+	var startYear = jsonData['startYear'];
+	var endDay = jsonData['endDay'];
+	var endMonth = jsonData['endMonth'];
+	var endYear = jsonData['endYear'];
+	var reserveStartTime = jsonData['reserveStartTime'];
+	var reserveEndTime = jsonData['reserveEndTime'];
+	var start_am_pm = jsonData['start_am_pm'];
+	var end_am_pm = jsonData['end_am_pm'];
+	var reserveDay = jsonData['reserveDay'];
+	var reserveMonth = jsonData['reserveMonth'];
+	var reserveYear = jsonData['reserveYear'];
+	var comment = jsonData['comment'];
+
+	if (jsonData['tripName'] != ""){
+		var sql = 'SELECT * FROM tripsTable WHERE tripName = ? AND userGuid = ?';
+		db.all(sql, [jsonData['tripName'], userId], function(error, row) {
+			if (row.length < 1 ){
+				sql = "INSERT INTO tripsTable (tripName, userGuid, tripGuid, startDay, startMonth, startYear, endDay,endMonth,endYear,tripInfo) VALUES (?,?,?,?,?,?,?,?,?,?)";
+				db.run(sql,[jsonData['tripName'],userId,tripGuid,startDay, startMonth, startYear, endDay,endMonth,endYear,jsonData['comment']], function(err, resp){
+					if (!err){
+						var itemGuid = uuid.v4();
+						var reserveTime = jsonData['reserveTime'];
+						var reserveDate = jsonData['reserveMonth']+"/"+jsonData['reserveDay']+"/"+jsonData["reserveYear"];
+						var comment = jsonData['comment']
+						var insert_into_itinerary = "INSERT INTO itineraryTable(tripGuid, placeId, itemGuid, reserveStartTime, reserveEndTime,start_am_pm,end_am_pm, reserveDay, reserveMonth, reserveYear, placeInfo) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+						db.run(insert_into_itinerary, [tripGuid, placeId, itemGuid, reserveStartTime, reserveEndTime,start_am_pm, end_am_pm,reserveDay,reserveMonth,reserveYear,comment], function(err, rows) {
+							if(err){
+								console.log(err);
+								response.write(JSON.stringify({"response":"failed", "userId":userId}))
+								response.send();
+							}else{
+								response.write(JSON.stringify({"response":"success", "userId":userId}));
+								response.send();
+							}
+						});
+						
+					}else{
+						console.log(err);
+						response.write(JSON.stringify({"response":"failed", "userId":userId}))
+						response.send();
+					}
+				});
+
+			}else{
+				response.write(JSON.stringify({"status":"failed"}));
+				response.send();
+			}
 		});
-		response.render('home', {'cities':cities});
+
+	}
+
+});
+
+
+router.get('/updateTripsTable', function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var jsonData = JSON.parse(queryObject.data);
+	var guid = queryObject.guid;
+	var userId = queryObject.userId;
+	var startDay = jsonData['startDay'];
+	var startMonth = jsonData['startMonth'];
+	var startYear = jsonData['startYear'];
+	var endDay = jsonData['endDay'];
+	var endMonth = jsonData['endMonth'];
+	var endYear = jsonData['endYear'];
+	var comment = jsonData['comment'];
+	var sql = "UPDATE tripsTable SET tripName = ?, startDay = ?, startMonth = ?, startYear = ?, endDay = ?, endMonth = ?, endYear = ?, tripInfo = ? WHERE tripGuid = ?";
+	db.run(sql, [jsonData['tripName'], startDay, startMonth, startYear, endDay, endMonth, endYear, comment, guid], function(error, rows){
+		if(!error){
+			response.write(JSON.stringify({"response":"success", "userId":userId}))
+			response.send()
+		}else{
+			console.log(error);
+			response.write(JSON.stringify({"response":"failed", "userId":userId}))
+		}
 	});
 });
 
+router.get('/displayTripItem', function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var id = queryObject.id;
+	var userId = queryObject.userId;
+	console.log(id);
+	sql = 'SELECT * FROM itineraryTable as itin LEFT JOIN placesTable as place ON place.Id = itin.placeId LEFT JOIN tripsTable as trip ON trip.tripGuid = itin.tripGuid WHERE itin.itemGuid = ?';
+	db.all(sql, id, function(error, rows){
+		if(!error){
+			console.log(rows);
+			response.render('displayTripItem', {'data':rows});
+		}else{
+			console.log(error);
+			response.write(JSON.stringify({"response":"failed", "userId":userId}));
+			response.send();
+		}
+	});
+});
+
+router.get('/selectTripView', function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var userId = queryObject.userId;
+	var tripGuid = queryObject.tripGuid;
+
+	sql = 'SELECT * FROM tripsTable WHERE tripGuid = ?';
+
+	db.all(sql, tripGuid, function (error, rows) {
+		if(!error){
+			response.render("deleteTripView", {'data':rows});
+		}else{
+			console.log(error);
+		}
+	});
+
+});
+
+router.get('/deleteTripAction', function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var userId = queryObject.userId;
+	var tripGuid = queryObject.tripGuid;
+
+	sql = 'DELETE FROM tripsTable WHERE tripGuid = ?';
+
+	db.run(sql, tripGuid, function (error, rows) {
+		if(!error){
+			console.log("Delete Trip Action")
+			response.write(JSON.stringify({"status":"success", "userId": userId}));
+			response.send();
+		}else{
+			console.log(error);
+			response.write(JSON.stringify({"status":"failed", "userId": userId}));
+			response.send();
+		}
+	});
+
+});
+
+router.get('/deleteItineraryItem', function(request, response){
+	const queryObject = url.parse(request.url, true).query;
+	var userId = queryObject.userId;
+	var itemGuid = queryObject.itemGuid;
+
+	sql = 'DELETE FROM itineraryTable WHERE itemGuid = ?';
+
+	db.run(sql, itemGuid, function (error, rows) {
+		if(!error){
+			console.log("Delete itinerary Action")
+			response.write(JSON.stringify({"status":"success", "userId": userId}));
+			response.send();
+		}else{
+			console.log(error);
+			response.write(JSON.stringify({"status":"failed", "userId": userId}));
+			response.send();
+		}
+	});
+
+});
+
+
+app.use('/', router);
 app.listen(8080, '0.0.0.0');
 console.log("Server is running on port 8080");
